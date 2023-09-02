26178e787eaced530481a46574b7af166c0563fe
moises-herrera <moisesherrera037@gmail.com>
2023-09-02 15:39:16 -0500

    style: add new post button

--- src/shared/components/LeftSidebarLinks.tsx
+++ src/shared/components/LeftSidebarLinks.tsx
@@ -1,11 +1,24 @@
 import { NavLink } from '.';
-import { LinkItems } from 'src/shared/services';
+import { linkItems } from 'src/shared/services';
 
 export const LeftSidebarLinks = () => {
   return (
     <nav className="flex flex-col text-white text-lg">
       <ul className="flex flex-col gap-2">
-        {LinkItems.map(({ ...props }) => (
+        {linkItems.slice(0, 2).map(({ ...props }) => (
+          <li key={props.path}>
+            <NavLink {...props} />
+          </li>
+        ))}
+
+        <button className="flex gap-2 items-center">
+          <span>
+            <i className="fa-solid fa-square-plus"></i>
+          </span>
+          <span>Nuevo Post</span>
+        </button>
+
+        {linkItems.slice(2).map(({ ...props }) => (
           <li key={props.path}>
             <NavLink {...props} />
           </li>
