53c7d32f211bc8b17ad5d6682f1cf7e3997ffa20
moises-herrera <moisesherrera037@gmail.com>
2023-09-02 22:49:00 -0500

    style: change main container styles

--- src/shared/components/MainContainer.tsx
+++ src/shared/components/MainContainer.tsx
@@ -3,5 +3,5 @@ interface MainContainerProps {
 }
 
 export const MainContainer = ({ children }: MainContainerProps) => {
-  return <div className="pl-[350px] h-full">{children}</div>;
+  return <div className="pl-[350px] h-full w-full">{children}</div>;
 };
