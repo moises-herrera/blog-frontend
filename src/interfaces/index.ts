cacc9237e9d6f1dc5ea9f4302f2c811909d39ad5
moises-herrera <moisesherrera037@gmail.com>
2023-09-02 22:48:29 -0500

    chore: add user and post models

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,7 @@ export * from './route-data.interface';
 export * from './types';
 export * from './auth-state.interface';
 export * from './link-item.interface';
+export * from './post.interface';
+export * from './user.interface';
+export * from './enums';
+export * from './post-info.interface';
---
+++ src/interfaces/post.interface.ts
@@ -0,0 +1,22 @@
+/**
+ * Represents the information of a post.
+ */
+export interface Post {
+  /** Post id. */
+  _id: string;
+
+  /** Post title. */
+  title: string;
+
+  /** Image url. */
+  image?: string;
+
+  /** Topic. */
+  topic: string;
+
+  /** Description. */
+  description: string;
+
+  /** User id of the author. */
+  userId: string;
+}
