f28740366c0d456ce93332c01cfffd6c991be670
moises-herrera <moisesherrera037@gmail.com>
2023-09-04 11:13:53 -0500

    refactor: sidebar style

--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -7,7 +7,7 @@ interface SidebarProps {
 export const Sidebar = ({ align = 'left', children }: SidebarProps) => {
   return (
     <aside
-      className={`fixed w-[350px] h-screen bg-[#2F2F2F] px-6 ${
+      className={`fixed z-50 w-[350px] h-screen bg-[#2F2F2F] px-6 ${
         align === 'right' ? 'right-0' : ''
       }`}
     >
