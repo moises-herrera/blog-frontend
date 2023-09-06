ea41b867313333ff7f37878ea39e8ec4fee027dc
moises-herrera <moisesherrera037@gmail.com>
2023-09-06 14:03:18 -0500

    feat: improve sign up schema

--- src/auth/validations/sign-up-schema.ts
+++ src/auth/validations/sign-up-schema.ts
@@ -1,25 +1,37 @@
+import { passwordPattern } from "src/helpers";
 import { z } from "zod";
 
 /**
  * Sign up form validation schema.
  */
-export const SignUpSchema = z.object({
-  firstName: z.string().nonempty("El nombre es requerido"),
-  lastName: z.string().nonempty("El apellido es requerido"),
-  username: z.string().nonempty("El usuario es requerido"),
-  email: z
-    .string()
-    .nonempty("El correo es requerido")
-    .email("El correo no es valido"),
-  password: z
-    .string()
-    .min(8, "La contraseña debe tener mínimo 8 carácteres")
-    .max(20, "La contraseña debe tener máximo 20 carácteres"),
-  confirmPassword: z
-    .string()
-    .min(8, "La contraseña debe tener mínimo 8 carácteres")
-    .max(20, "La contraseña debe tener máximo 20 carácteres"),
-});
+export const SignUpSchema = z
+  .object({
+    firstName: z.string().nonempty("El nombre es requerido"),
+    lastName: z.string().nonempty("El apellido es requerido"),
+    username: z.string().nonempty("El usuario es requerido"),
+    email: z
+      .string()
+      .nonempty("El correo es requerido")
+      .email("El correo no es valido"),
+    password: z
+      .string()
+      .min(8, "La contraseña debe tener mínimo 8 carácteres")
+      .max(20, "La contraseña debe tener máximo 20 carácteres")
+      .regex(
+        passwordPattern,
+        "La contraseña debe tener al menos una mayúscula, una minúscula y un número"
+      ),
+    confirmPassword: z.string().nonempty("La confirmación es requerida"),
+  })
+  .superRefine(({ password, confirmPassword }, context) => {
+    if (password !== confirmPassword) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ["confirmPassword"],
+        message: "Las contraseñas no coinciden",
+      });
+    }
+  });
 
 /**
  * Type of the sign up form validation schema.
