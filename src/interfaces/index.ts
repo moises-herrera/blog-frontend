0cc45ec88cd681d627feaff299354ccff3587043
moises-herrera <moisesherrera037@gmail.com>
2023-09-07 13:43:02 -0500

    chore: add models

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -16,3 +16,5 @@ export * from "./send-email.interface";
 export * from "./email-state.interface";
 export * from "./standard-response.interface";
 export * from "./error-response.interface";
+export * from "./standard-object.interface";
+export * from "./change-password.interface";
