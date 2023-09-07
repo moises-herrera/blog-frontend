e6087c7478c7e324a917ff5b613da1e502b58a02
moises-herrera <moisesherrera037@gmail.com>
2023-09-07 17:57:23 -0500

    refactor: settings form schema

--- src/settings/validations/setting-schema.ts
+++ src/settings/validations/setting-schema.ts
@@ -2,7 +2,7 @@ import { passwordPattern } from "src/helpers";
 import { z } from "zod";
 
 /**
- * Sign up form validation schema.
+ * Settings form validation schema.
  */
 export const SettingSchema = z.object({
   firstName: z.string().nonempty("El nombre es requerido"),
@@ -13,17 +13,22 @@ export const SettingSchema = z.object({
     .string()
     .nonempty("El correo es requerido")
     .email("El correo no es valido"),
-  // password: z
-  //   .string()
-  //   .min(8, "La contraseña debe tener mínimo 8 carácteres")
-  //   .max(20, "La contraseña debe tener máximo 20 carácteres")
-  //   .regex(
-  //     passwordPattern,
-  //     "La contraseña debe tener al menos una mayúscula, una minúscula y un número"
-  //   ),
+  password: z
+    .union([
+      z.string().length(0),
+      z
+        .string()
+        .min(8, "La contraseña debe tener mínimo 8 carácteres")
+        .max(20, "La contraseña debe tener máximo 20 carácteres")
+        .regex(
+          passwordPattern,
+          "La contraseña debe tener al menos una mayúscula, una minúscula y un número"
+        ),
+    ])
+    .optional(),
 });
 
 /**
- * Type of the sign up form validation schema.
+ * Type of the settings form validation schema.
  */
 export type SettingSchemaType = z.infer<typeof SettingSchema>;
