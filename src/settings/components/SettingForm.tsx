4ce91959338ab36b1ff353ee3f9778d857049805
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-09-07 17:15:56 -0500

    Review SettingForm

--- src/settings/components/SettingForm.tsx
+++ src/settings/components/SettingForm.tsx
@@ -1,11 +1,34 @@
-import { Box, Image, Input } from "@chakra-ui/react";
-import { ButtonComponent } from "src/shared/components";
+import { Box, Image, Input, Button } from "@chakra-ui/react";
 import { useRef } from "react";
+
+//New imports
+import { useTypedSelector } from "src/store";
+import { SettingSchema, SettingSchemaType } from "src/settings/validations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { FormControlContainer } from "src/shared/components";
+import { User } from "src/interfaces";
 
 export const SettingForm = () => {
   const profileImageInputRef = useRef<HTMLInputElement | null>(null);
   const onUploadImage = (): void => {
     profileImageInputRef.current?.click();
+  };
+  //Gabriel
+  const { user } = useTypedSelector(({ auth }) => auth);
+  const { password, ...defaultValues } = user as User;
+  const {
+    setValue,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<SettingSchemaType>({
+    defaultValues: defaultValues,
+    resolver: zodResolver(SettingSchema),
+  });
+
+  const onSubmitForm: SubmitHandler<SettingSchemaType> = (data) => {
+    console.log(data);
   };
 
   return (
@@ -19,9 +42,10 @@ export const SettingForm = () => {
             alt="Dan Abramov"
             cursor="pointer"
             onClick={onUploadImage}
-            src="https://bit.ly/dan-abramov"
+            src={""}
           />
           <Input
+            onChange={({ target: { files } }) => setValue("avatar", files?.[0])}
             className="hidden"
             type="file"
             accept="image/png,image/jpg, image/jpeg"
@@ -29,22 +53,48 @@ export const SettingForm = () => {
           />
         </Box>
         <div className="flex justify-center w-full mt-5">
-          <form className="flex flex-col w-full gap-10 px-5 lg:w-1/2 lg:px-0">
-            <Input value="Gabriel" placeholder="Nombre" variant="settings" />
-            <Input value="Oquendo" placeholder="Apellido" variant="settings" />
-            <Input
-              value="@Oquendo_Gabriel"
-              placeholder="Usuario"
-              variant="settings"
-            />
-            <Input
-              value="oquendogabrielq8@gmail.com"
-              placeholder="Correo"
-              variant="settings"
-            />
+          <form
+            onSubmit={handleSubmit(onSubmitForm)}
+            className="flex flex-col w-full gap-10 px-5 lg:w-1/2 lg:px-0"
+          >
+            <FormControlContainer fieldError={errors.firstName}>
+              <Input
+                type="text"
+                defaultValue={defaultValues.firstName}
+                placeholder="Nombre"
+                variant="settings"
+                {...register("firstName")}
+              />
+            </FormControlContainer>
+            <FormControlContainer fieldError={errors.lastName}>
+              <Input
+                type="text"
+                defaultValue={defaultValues.lastName}
+                placeholder="Apellido"
+                variant="settings"
+                {...register("lastName")}
+              />
+            </FormControlContainer>
+            <FormControlContainer fieldError={errors.username}>
+              <Input
+                type="text"
+                defaultValue={defaultValues.username}
+                placeholder="Usuario"
+                variant="settings"
+                {...register("username")}
+              />
+            </FormControlContainer>
+            <FormControlContainer fieldError={errors.email}>
+              <Input
+                type="text"
+                defaultValue={defaultValues.email}
+                placeholder="Correo"
+                variant="settings"
+                {...register("email")}
+              />
+            </FormControlContainer>
             <div className="flex">
-              <ButtonComponent title="Cancelar" />
-              <ButtonComponent title="Guardar" />
+              <Button type="submit">Submit</Button>
             </div>
           </form>
         </div>
