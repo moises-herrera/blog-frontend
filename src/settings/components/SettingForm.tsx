2de6fdbec3e58ed87e657f8a0dcf5d0b6de11bcc
moises-herrera <moisesherrera037@gmail.com>
2023-09-07 17:58:09 -0500

    fix: settings form validation

--- src/settings/components/SettingForm.tsx
+++ src/settings/components/SettingForm.tsx
@@ -7,7 +7,7 @@ import { SettingSchema, SettingSchemaType } from "src/settings/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { FormControlContainer } from "src/shared/components";
-import { User } from "src/interfaces";
+import avatarPlaceholder from "src/assets/images/avatar-placeholder.png";
 
 export const SettingForm = () => {
   const profileImageInputRef = useRef<HTMLInputElement | null>(null);
@@ -16,14 +16,17 @@ export const SettingForm = () => {
   };
   //Gabriel
   const { user } = useTypedSelector(({ auth }) => auth);
-  const { password, ...defaultValues } = user as User;
+  const formDefaultValues = {
+    ...user,
+    password: "",
+  };
   const {
     setValue,
     handleSubmit,
     register,
     formState: { errors },
   } = useForm<SettingSchemaType>({
-    defaultValues: defaultValues,
+    defaultValues: formDefaultValues,
     resolver: zodResolver(SettingSchema),
   });
 
@@ -32,35 +35,34 @@ export const SettingForm = () => {
   };
 
   return (
-    <div className="w-full h-screen bg-[#D3D3D3] text-[#2F2F2F]">
+    <div className="w-full h-screen bg-secondary-200 text-primary-500">
       <div className="flex flex-col items-center justify-center h-screen">
         <Box>
           <Image
             content="center"
             borderRadius="full"
             boxSize="200px"
-            alt="Dan Abramov"
+            alt="avatar"
             cursor="pointer"
             onClick={onUploadImage}
-            src={""}
+            src={user?.avatar || avatarPlaceholder}
           />
           <Input
             onChange={({ target: { files } }) => setValue("avatar", files?.[0])}
             className="hidden"
             type="file"
-            accept="image/png,image/jpg, image/jpeg"
+            accept="image/png,image/jpg,image/jpeg"
             ref={profileImageInputRef}
           />
         </Box>
         <div className="flex justify-center w-full mt-5">
           <form
             onSubmit={handleSubmit(onSubmitForm)}
-            className="flex flex-col w-full gap-10 px-5 lg:w-1/2 lg:px-0"
+            className="flex flex-col w-full gap-10 px-12 lg:w-1/2 lg:px-0"
           >
             <FormControlContainer fieldError={errors.firstName}>
               <Input
                 type="text"
-                defaultValue={defaultValues.firstName}
                 placeholder="Nombre"
                 variant="settings"
                 {...register("firstName")}
@@ -69,7 +71,6 @@ export const SettingForm = () => {
             <FormControlContainer fieldError={errors.lastName}>
               <Input
                 type="text"
-                defaultValue={defaultValues.lastName}
                 placeholder="Apellido"
                 variant="settings"
                 {...register("lastName")}
@@ -78,7 +79,6 @@ export const SettingForm = () => {
             <FormControlContainer fieldError={errors.username}>
               <Input
                 type="text"
-                defaultValue={defaultValues.username}
                 placeholder="Usuario"
                 variant="settings"
                 {...register("username")}
@@ -87,14 +87,27 @@ export const SettingForm = () => {
             <FormControlContainer fieldError={errors.email}>
               <Input
                 type="text"
-                defaultValue={defaultValues.email}
                 placeholder="Correo"
                 variant="settings"
                 {...register("email")}
               />
             </FormControlContainer>
-            <div className="flex">
-              <Button type="submit">Submit</Button>
+            <FormControlContainer fieldError={errors.password}>
+              <Input
+                type="password"
+                placeholder="Contraseña"
+                variant="settings"
+                {...register("password")}
+              />
+            </FormControlContainer>
+            <div className="flex justify-center space-x-8">
+              <Button type="button" variant="form">
+                Cancelar
+              </Button>
+
+              <Button type="submit" variant="form">
+                Guardar
+              </Button>
             </div>
           </form>
         </div>
