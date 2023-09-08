c59afeb4191ec5e0f4a0b5d772264328ec7b7ea1
moises-herrera <moisesherrera037@gmail.com>
2023-09-07 21:04:02 -0500

    refactor: user card

--- src/shared/components/UserCard.tsx
+++ src/shared/components/UserCard.tsx
@@ -1,26 +1,24 @@
 import { Avatar } from "@chakra-ui/react";
+import { User } from "src/interfaces";
+import { getFullName } from "src/helpers";
 
-interface Props {
-  user: string;
-  img: string;
+interface UserCardProps {
+  user: User;
   children: React.ReactNode;
 }
-export const UserCard = ({ children, user, img }: Props) => {
+
+export const UserCard = ({ children, user }: UserCardProps) => {
   return (
-    <>
-      <div className="flex max-w-full h-[54px] bg-white rounded-lg mb-2 justify-between mr-1">
-        <div className="flex items-center">
-          <Avatar
-            marginLeft={"10px"}
-            marginRight={"5px"}
-            size="md"
-            name="Non"
-            src={img}
-          />
-          <p>{user}</p>
-        </div>
-        <div className="flex items-center max-w-full mr-2">{children}</div>
+    <div className="flex max-w-full bg-white rounded-lg justify-between p-3">
+      <div className="flex items-center gap-3">
+        <Avatar
+          size="md"
+          name={getFullName(user)}
+          src={user.avatar}
+        />
+        <p>@{user.username}</p>
       </div>
-    </>
+      <div className="flex items-center max-w-full">{children}</div>
+    </div>
   );
 };
