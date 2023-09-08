4f493400dfb45a5d3f14caeb2ac518406bc68536
moises-herrera <moisesherrera037@gmail.com>
2023-09-08 18:13:44 -0500

    feat: display user posts

--- src/profile/pages/Profile.tsx
+++ src/profile/pages/Profile.tsx
@@ -1,14 +1,35 @@
-import { FeedContent } from 'src/feed/components';
-import { posts } from 'src/mocks';
-import { ProfileHeader } from 'src/profile/components';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { FeedContent } from "src/feed/components";
+import { ProfileHeader } from "src/profile/components";
+import { Loading } from "src/shared/components";
+import { useTypedSelector } from "src/store";
+import { getUserPosts } from "src/store/post";
+import { AppDispatch } from "src/store/types";
 
 export const Profile = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useTypedSelector(({ auth }) => auth);
+  const { userPosts, isLoadingUserPosts } = useTypedSelector(
+    ({ post }) => post
+  );
+
+  useEffect(() => {
+    dispatch(getUserPosts(user?._id as string));
+  }, []);
+
   return (
     <section className="section-content">
       <ProfileHeader />
 
       <div className="mt-[300px] lg:mt-28">
-        <FeedContent posts={posts} />
+        {!isLoadingUserPosts ? (
+          <FeedContent posts={userPosts} />
+        ) : (
+          <div className="loading-container">
+            <Loading textClass="text-black" />
+          </div>
+        )}
       </div>
     </section>
   );
