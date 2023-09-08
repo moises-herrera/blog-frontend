c33ea2d2e9528e0be02917a1b65cd03771037957
moises-herrera <moisesherrera037@gmail.com>
2023-09-07 22:05:18 -0500

    refactor: display loading in right sidebar

--- src/shared/components/RightSidebar.tsx
+++ src/shared/components/RightSidebar.tsx
@@ -1,24 +1,30 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { FollowButton, UserCard, Sidebar } from "src/shared/components";
+import {
+  FollowButton,
+  UserCard,
+  Sidebar,
+  Loading,
+} from "src/shared/components";
 import { useTypedSelector } from "src/store";
 import { AppDispatch } from "src/store/types";
 import { getFollowers, getFollowing } from "src/store/users";
 
-interface Props {
-  onOpen: () => void;
-}
-
-export const RightSidebar = ({ onOpen }: Props) => {
+export const RightSidebar = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { user } = useTypedSelector(({ auth }) => auth);
-  const { followers, following } = useTypedSelector(({ users }) => users);
+  const { followers, following, followersLoading, followingLoading } =
+    useTypedSelector(({ users }) => users);
 
   useEffect(() => {
     dispatch(getFollowers(user?._id as string));
     dispatch(getFollowing(user?._id as string));
   }, []);
+
+  const onClickFollow = () => {};
+
+  const onClickUnfollow = () => {};
 
   return (
     <Sidebar align="right" cssClass="hidden lg:block">
@@ -38,21 +44,29 @@ export const RightSidebar = ({ onOpen }: Props) => {
         <p>Seguidos</p>
       </div>
       <div className="users-list h-[310px] overflow-auto scrollable-div">
-        {following.map((user) => (
-          <UserCard key={user.username} user={user}>
-            <FollowButton title="No seguir" onOpen={onOpen} />
-          </UserCard>
-        ))}
+        {!followingLoading ? (
+          following.map((user) => (
+            <UserCard key={user.username} user={user}>
+              <FollowButton title="No seguir" onOpen={onClickUnfollow} />
+            </UserCard>
+          ))
+        ) : (
+          <Loading />
+        )}
       </div>
       <div className="text-[#E0E0E0] pt-5 text-[30px] font-bold pb-3">
         <p>Seguidores</p>
       </div>
       <div className="users-list h-[310px] overflow-auto scrollable-div">
-        {followers.map((user) => (
-          <UserCard key={user.username} user={user}>
-            <FollowButton title="Seguir" onOpen={onOpen} />
-          </UserCard>
-        ))}
+        {!followersLoading ? (
+          followers.map((user) => (
+            <UserCard key={user.username} user={user}>
+              <FollowButton title="Seguir" onOpen={onClickFollow} />
+            </UserCard>
+          ))
+        ) : (
+          <Loading />
+        )}
       </div>
     </Sidebar>
   );
