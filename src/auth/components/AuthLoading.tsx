4b751f06c2a44e5a22908d9568612115a236c90e
moises-herrera <moisesherrera037@gmail.com>
2023-09-07 22:04:51 -0500

    refactor: add loading component

--- src/auth/components/AuthLoading.tsx
+++ src/auth/components/AuthLoading.tsx
@@ -1,17 +1,9 @@
-import { Spinner } from "@chakra-ui/react";
+import { Loading } from "src/shared/components";
 
 export const AuthLoading = () => {
   return (
     <section className="flex flex-col gap-4 w-full h-screen bg-primary-500 justify-center items-center">
-      <Spinner
-        thickness="4px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="accent.500"
-        w="100px"
-        h="100px"
-      />
-      <p className="text-lg text-white">Cargando</p>
+      <Loading />
     </section>
   );
 };
