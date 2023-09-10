ea6a046b64dc981d6a59fd9526f482fb111eccce
moises-herrera <moisesherrera037@gmail.com>
2023-09-10 14:33:10 -0500

    refactor: comment card

---
+++ src/post/components/CommentCard.tsx
@@ -0,0 +1,54 @@
+import { Avatar } from "@chakra-ui/react";
+import { useTypedSelector } from "src/store";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "src/store/types";
+import { SettingsMenu } from "src/shared/components";
+import { openDeleteModal } from "src/store/comment";
+
+interface Props {
+  commentId: string;
+  username: string;
+  content: string;
+  avatar: string;
+  postAuthorId: string;
+  commentAuthorId: string;
+}
+
+export const CommentCard = ({
+  commentId,
+  username,
+  content,
+  avatar,
+  postAuthorId,
+  commentAuthorId,
+}: Props) => {
+  const { user: currentUser } = useTypedSelector(({ auth }) => auth);
+  const dispatch = useDispatch<AppDispatch>();
+  const hasPermissions = currentUser?._id === commentAuthorId;
+  const isOwner = currentUser?._id === postAuthorId;
+
+  const onClickDelete = () => {
+    dispatch(openDeleteModal(commentId));
+  };
+
+  return (
+    <div className="flex mb-4">
+      <div className="px-4">
+        <Avatar size="md" src={avatar} />
+      </div>
+      <div className="flex flex-col w-full text-white">
+        <div className="flex justify-between w-full pr-4">
+          <p className="font-bold text-[16px]">{username}</p>
+          <SettingsMenu
+            variant="dark"
+            canUpdate={false}
+            canDelete={hasPermissions || isOwner}
+            onClickUpdate={() => {}}
+            onClickDelete={onClickDelete}
+          />
+        </div>
+        <p className="pr-3 text-[14px]">{content}</p>
+      </div>
+    </div>
+  );
+};
