a079d8710443f316de9fa5c32217fee21f083251
moises-herrera <moisesherrera037@gmail.com>
2023-09-10 14:32:42 -0500

    refactor: settings menu props

--- src/shared/components/SettingsMenu.tsx
+++ src/shared/components/SettingsMenu.tsx
@@ -1,15 +1,19 @@
 import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 
 interface SettingsMenuProps {
+  variant?: string;
+  canUpdate?: boolean;
+  canDelete?: boolean;
   onClickUpdate: () => void;
   onClickDelete: () => void;
-  variant?: string;
 }
 
 export const SettingsMenu = ({
+  variant = "outline",
+  canUpdate = true,
+  canDelete = true,
   onClickUpdate,
   onClickDelete,
-  variant = "outline",
 }: SettingsMenuProps) => {
   return (
     <Menu variant={variant}>
@@ -17,18 +21,22 @@ export const SettingsMenu = ({
         <i className="text-xl fa-solid fa-ellipsis-vertical"></i>
       </MenuButton>
       <MenuList>
-        <MenuItem
-          onClick={onClickUpdate}
-          icon={<i className="fa-solid fa-pen-to-square"></i>}
-        >
-          Editar
-        </MenuItem>
-        <MenuItem
-          onClick={onClickDelete}
-          icon={<i className="fa-solid fa-trash"></i>}
-        >
-          Eliminar
-        </MenuItem>
+        {canUpdate && (
+          <MenuItem
+            onClick={onClickUpdate}
+            icon={<i className="fa-solid fa-pen-to-square"></i>}
+          >
+            Editar
+          </MenuItem>
+        )}
+        {canDelete && (
+          <MenuItem
+            onClick={onClickDelete}
+            icon={<i className="fa-solid fa-trash"></i>}
+          >
+            Eliminar
+          </MenuItem>
+        )}
       </MenuList>
     </Menu>
   );
