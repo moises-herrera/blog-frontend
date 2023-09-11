181f31abe76fa3de851f4fcf122311807aee6f64
moises-herrera <moisesherrera037@gmail.com>
2023-09-11 12:37:25 -0500

    style: change settings form style

--- src/settings/components/SettingForm.tsx
+++ src/settings/components/SettingForm.tsx
@@ -103,83 +103,83 @@ export const SettingForm = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen bg-secondary-200 text-primary-500">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <Box>
-          <Image
-            content="center"
-            borderRadius="full"
-            boxSize="200px"
-            alt="avatar"
-            cursor="pointer"
-            onClick={onUploadImage}
-            src={(userAvatar as string) || avatarPlaceholder}
-          />
-          <Input
-            onChange={({ target: { files } }) => onUploadAvatar(files)}
-            className="hidden"
-            type="file"
-            accept="image/png,image/jpg,image/jpeg"
-            ref={profileImageInputRef}
-          />
-        </Box>
-        <div className="flex justify-center w-full mt-5">
-          <form
-            onSubmit={handleSubmit(onSubmitForm)}
-            className="flex flex-col w-full gap-10 px-12 lg:w-1/2 lg:px-0"
-          >
-            <FormControlContainer fieldError={errors.firstName}>
-              <Input
-                type="text"
-                placeholder="Nombre"
-                variant="settings"
-                {...register("firstName")}
-              />
-            </FormControlContainer>
-            <FormControlContainer fieldError={errors.lastName}>
-              <Input
-                type="text"
-                placeholder="Apellido"
-                variant="settings"
-                {...register("lastName")}
-              />
-            </FormControlContainer>
-            <FormControlContainer fieldError={errors.username}>
-              <Input
-                type="text"
-                placeholder="Usuario"
-                variant="settings"
-                {...register("username")}
-              />
-            </FormControlContainer>
-            <FormControlContainer fieldError={errors.email}>
-              <Input
-                type="text"
-                placeholder="Correo"
-                variant="settings"
-                {...register("email")}
-              />
-            </FormControlContainer>
-            <FormControlContainer fieldError={errors.password}>
-              <Input
-                type="password"
-                placeholder="Contraseña"
-                variant="settings"
-                {...register("password")}
-              />
-            </FormControlContainer>
-            <div className="flex justify-center space-x-8">
-              <Button type="button" variant="form" onClick={onCancelChanges}>
-                Cancelar
-              </Button>
+    <section className="w-full min-h-screen bg-secondary-200 text-primary-500 py-5 flex flex-col justify-center">
+      <Box className="flex justify-center mb-5">
+        <Image
+          content="center"
+          borderRadius="full"
+          alt="avatar"
+          cursor="pointer"
+          onClick={onUploadImage}
+          className="w-[150px] md:w-[200px]"
+          src={(userAvatar as string) || avatarPlaceholder}
+        />
+        <Input
+          onChange={({ target: { files } }) => onUploadAvatar(files)}
+          className="hidden"
+          type="file"
+          accept="image/png,image/jpg,image/jpeg"
+          ref={profileImageInputRef}
+        />
+      </Box>
 
-              <Button type="submit" variant="form" isLoading={isLoading}>
-                Guardar
-              </Button>
-            </div>
-          </form>
-        </div>
+      <div className="w-full flex justify-center">
+        <form
+          onSubmit={handleSubmit(onSubmitForm)}
+          className="flex flex-col w-full gap-10 px-5 md:px-12 lg:w-1/2 lg:px-0"
+        >
+          <FormControlContainer fieldError={errors.firstName}>
+            <Input
+              type="text"
+              placeholder="Nombre"
+              variant="settings"
+              {...register("firstName")}
+            />
+          </FormControlContainer>
+          <FormControlContainer fieldError={errors.lastName}>
+            <Input
+              type="text"
+              placeholder="Apellido"
+              variant="settings"
+              {...register("lastName")}
+            />
+          </FormControlContainer>
+          <FormControlContainer fieldError={errors.username}>
+            <Input
+              type="text"
+              placeholder="Usuario"
+              variant="settings"
+              {...register("username")}
+            />
+          </FormControlContainer>
+          <FormControlContainer fieldError={errors.email}>
+            <Input
+              type="text"
+              placeholder="Correo"
+              variant="settings"
+              {...register("email")}
+            />
+          </FormControlContainer>
+          <FormControlContainer fieldError={errors.password}>
+            <Input
+              type="password"
+              placeholder="Contraseña"
+              variant="settings"
+              {...register("password")}
+            />
+          </FormControlContainer>
+
+          <div className="flex justify-center space-x-2 md:space-x-8">
+            <Button type="button" variant="form" onClick={onCancelChanges}>
+              Cancelar
+            </Button>
+
+            <Button type="submit" variant="form" isLoading={isLoading}>
+              Guardar
+            </Button>
+          </div>
+        </form>
       </div>
-    </div>
+    </section>
   );
 };
