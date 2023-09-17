521ed6fa598e99a4e93b497c8965bd9f2d03a0b4
moises-herrera <moisesherrera037@gmail.com>
2023-09-16 21:56:17 -0500

    feat: filter chats

--- src/chats/components/ChatContainer.tsx
+++ src/chats/components/ChatContainer.tsx
@@ -1,31 +1,36 @@
-import { Input, InputGroup, InputLeftElement, Button } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { ChatItem } from ".";
 import { useTypedSelector } from "src/store";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "src/store/types";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { getChatsList } from "src/store/chats";
-import { useScrollPagination } from "src/hooks";
+import { useScrollPagination, useSearch } from "src/hooks";
 
 export const ChatContainer = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { list, isLoadingList, totalChats } = useTypedSelector(
     ({ chats }) => chats
   );
+  const { debouncedSearchTerm, onSearch } = useSearch({ value: "" });
+  const listRef = useRef<HTMLDivElement>(null);
+
   const { page } = useScrollPagination({
     isLoading: isLoadingList,
     currentRecords: list.length,
     total: totalChats,
+    elementRef: listRef,
   });
 
   useEffect(() => {
     dispatch(
       getChatsList({
+        search: debouncedSearchTerm || "",
         limit: 10,
         page,
       })
     );
-  }, [dispatch, page]);
+  }, [dispatch, debouncedSearchTerm, page]);
 
   return (
     <div className="w-full h-screen bg-secondary-200 md:w-1/2 lg:w-1/3 xl:w-1/2 2xl:w-1/3">
@@ -33,11 +38,12 @@ export const ChatContainer = () => {
         <form className="w-full ">
           <InputGroup>
             <InputLeftElement>
-              <Button backgroundColor={"#ffffff"} marginLeft={"10px"}>
+              <button className="ml-[10px]">
                 <i className="fa-solid fa-magnifying-glass"></i>
-              </Button>
+              </button>
             </InputLeftElement>
             <Input
+              onChange={onSearch}
               type="text"
               placeholder="Buscar..."
               marginLeft={"5px"}
@@ -45,9 +51,12 @@ export const ChatContainer = () => {
             />
           </InputGroup>
         </form>
-        <i className="pl-2 pt-2 text-2xl fa-solid fa-pencil text-[##e9ecf1]"></i>
+        <i className="pl-2 pt-2 text-2xl fa-solid fa-pencil"></i>
       </div>
-      <div className="w-full px-3 overflow-auto chat-list scrollable-chat">
+      <div
+        ref={listRef}
+        className="w-full px-3 overflow-auto chat-list scrollable-chat"
+      >
         {list.map((chat) => (
           <ChatItem key={chat._id} {...chat} />
         ))}
