8a61e91e1b9ee09d57e6c76d1981091a3fc08c0c
moises-herrera <moisesherrera037@gmail.com>
2023-09-16 21:09:38 -0500

    chore: add user chat model

---
+++ src/interfaces/user-chat.interface.ts
@@ -0,0 +1,16 @@
+/**
+ * Represents the information of a user in a chat.
+ */
+export interface UserChat {
+  /** User id. */
+  _id: string;
+
+  /** First name. */
+  firstName: string;
+
+  /** Last name. */
+  lastName: string;
+
+  /** Avatar. */
+  avatar: string;
+}
