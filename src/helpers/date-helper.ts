34aa0f3040756336bbabbbc27a594deb371b565a
moises-herrera <moisesherrera037@gmail.com>
2023-09-17 12:47:23 -0500

    chore: add get time helper

--- src/helpers/date-helper.ts
+++ src/helpers/date-helper.ts
@@ -8,3 +8,16 @@ export const getDateFormattedFromString = (date: Date | string): string => {
   const dateFormatted = new Date(date).toLocaleDateString();
   return dateFormatted;
 };
+
+/**
+ * Get time formatted from date.
+ *
+ * @param date Date to format.
+ * @returns Time formatted.
+ */
+export const getTimeFormatted = (date: Date | string): string => {
+  const newDate = new Date(date);
+  const minutes = newDate.getMinutes();
+
+  return `${newDate.getHours()}:${minutes < 10 ? `0${minutes}` : minutes}`;
+};
